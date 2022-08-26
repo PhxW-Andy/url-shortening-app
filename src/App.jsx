f954ac403f82bd78446c080feb5b6a12ce7cc5f8
Andre Lebioda <50991147+PhxW-Andy@users.noreply.github.com>
2022-08-26 02:27:28 +0200

    add navigation and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import '../src/scss/style.scss'
+// import components
+import Nav from "./components/Nav";
+
+// import styles
+import "../src/scss/style.scss";
 
 const App = () => {
   return (
-    <div className="container">
-      <h1>Vite reactjs boilerplate </h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores,
-        necessitatibus sed culpa vero veritatis architecto aliquid laborum ipsum
-        ducimus debitis facere maxime, iure perferendis doloribus minima quia
-        commodi vitae!
-      </p>
+    <div className="container-lg">
+      <Nav />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,34 @@
+import Logo from "../assets/images/nav/logo.svg";
+
+const Nav = () => {
+  return (
+    <nav>
+      <div className="container">
+        <div className="content-wrapper">
+          <div className="logo">
+            <img src={Logo} alt="Shortly Logo" />
+          </div>
+          <div className="nav-wrapper">
+            <div className="nav-primary">
+              <ul>
+                <li>Features</li>
+                <li>Pricing</li>
+                <li>Resources</li>
+              </ul>
+            </div>
+            <div className="nav-secondary">
+              <ul>
+                <li>Login</li>
+                <li>
+                  <button className="btn-sign-up">Sign Up</button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
