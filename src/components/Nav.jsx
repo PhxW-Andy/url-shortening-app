c4667aec1e49095b58dc33e21c3741c5a9f12130
Andre Lebioda <50991147+PhxW-Andy@users.noreply.github.com>
2022-09-01 18:06:05 +0200

    add mobile nav and general mobile styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
+
+//
 import Logo from "../assets/images/nav/logo.svg";
 
 const Nav = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle((prev) => !prev);
+  };
+
   return (
     <nav>
       <div className="container">
@@ -8,7 +17,7 @@ const Nav = () => {
           <div className="logo">
             <img src={Logo} alt="Shortly Logo" />
           </div>
-          <div className="nav-wrapper">
+          <div className={`nav-wrapper ${toggle ? "show" : ""}`}>
             <div className="nav-primary">
               <ul>
                 <li>Features</li>
@@ -27,6 +36,14 @@ const Nav = () => {
               </ul>
             </div>
           </div>
+          <button
+            className={`toggle ${toggle ? "show" : ""}`}
+            onClick={handleToggle}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
         </div>
       </div>
     </nav>
